038ba89a5f724196da6bc5da6bb4059d76f38113
Daniel Mott <daniel.r.mott@gmail.com>
2024-02-24 12:38:18 -0800

    test

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 document.addEventListener("keydown", checkUpAndDown, false);
 document.addEventListener("keyup", checkUpAndDown, false);
 
-// Takes str from keydown and adds it to "#key?", then toggles the classes for that key id
+// Takes str from keydown and adds it to "#key", then toggles the classes for that key id
 function checkUpAndDown(e) {
-  if (e.repeat) {
-    return;
-  }
+//   if (e.repeat) {
+//     return;
+//   }
   const selectorString = "#key" + String.fromCharCode(e.keyCode);
   const pressedKeyElement = document.querySelector(selectorString);
   pressedKeyElement.classList.toggle("key");
   pressedKeyElement.classList.toggle("key-pressed");
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+
+
 };
+
+// need to make no sound on keyup
