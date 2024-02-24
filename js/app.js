5522d616472ae15607ff919cfe05c4830ff2dcfd
Daniel Mott <daniel.r.mott@gmail.com>
2024-02-24 00:49:11 -0800

    finalized keydown function

--- js/app.js
+++ js/app.js
@@ -1,58 +1,13 @@
-const keyPressed = document.querySelector(".key-pressed");
-const keyDiv = document.querySelector(".key-wrapper");
-const keyA = document.querySelector("#keyA");
-const keyS = document.querySelector("#keyS");
-const keyD = document.querySelector("#keyD");
-const keyF = document.querySelector("#keyF");
-const keyG = document.querySelector("#keyG");
-const keyH = document.querySelector("#keyH");
-const keyJ = document.querySelector("#keyJ");
-const keyK = document.querySelector("#keyK");
-const keyL = document.querySelector("#keyL");
-const key = document.querySelector(".key");
+document.addEventListener("keydown", checkUpAndDown, false);
+document.addEventListener("keyup", checkUpAndDown, false);
 
-document.addEventListener("keydown", checkKeyPressed, false); 
-
-// KEYCODES FOR BUTTONS
-// A = 65
-// S = 83
-// D = 68
-// F = 70
-// G = 71
-// H = 72
-// J = 74
-// K = 75
-// L = 76
-
-// not sure if i need this yet
-const keyArray = [];
-const keyIds = document.querySelectorAll(`[id]`);
-keyIds.forEach(element => {
-keyArray.push(element.id);
-});
-
-
-// still figuring this out /////////////////////
-const classId = key.classList;
-
-function checkKeyPressed(e) {
-    if (e.keyCode === 65) {
-        keyA.classList.remove("key");
-        keyA.classList.add("key-pressed");
-        setTimeout(() => {
-        keyA.classList.remove("key-pressed");
-        keyA.classList.add("key");
-          }, 100);
-    }
+// Takes str from keydown and adds it to "#key?", then toggles the classes for that key id
+function checkUpAndDown(e) {
+  if (e.repeat) {
+    return;
+  }
+  const selectorString = "#key" + String.fromCharCode(e.keyCode);
+  const pressedKeyElement = document.querySelector(selectorString);
+  pressedKeyElement.classList.toggle("key");
+  pressedKeyElement.classList.toggle("key-pressed");
 };
-////////////////////////////////////////////////
-
-
-
-
-//saving this for later
-// setTimeout(() => {
-//     // Sets timeout to 5 seconds -- add stuff here
-//   }, 5000);
-
-
