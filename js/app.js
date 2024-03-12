944a175414ab4253833d80b9e8f45f76fa7b00fb
Daniel-R-Mott <daniel.r.mott@gmail.com>
2024-03-12 16:52:37 -0700

    added all sounds, background

--- js/app.js
+++ js/app.js
@@ -3,18 +3,34 @@ document.addEventListener("keyup", checkUpAndDown, false);
 
 // Takes str from keydown and adds it to "#key", then toggles the classes for that key id
 function checkUpAndDown(e) {
-//   if (e.repeat) {
-//     return;
-//   }
+  if (e.repeat) {
+    return;
+  }
   const selectorString = "#key" + String.fromCharCode(e.keyCode);
   const pressedKeyElement = document.querySelector(selectorString);
   pressedKeyElement.classList.toggle("key");
   pressedKeyElement.classList.toggle("key-pressed");
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  audio.currentTime = 0;
-  audio.play();
+  console.log(e.keyCode);
 
 
+
+  if (pressedKeyElement.classList.contains("key-pressed")) {
+    playAudio(e.keyCode);
+  }
 };
 
-// need to make no sound on keyup
+function playAudio(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+};
+
+
+// S = 83
+// D = 68
+// F = 70
+// G = 71
+// H = 72
+// J = 74
+// K = 75
+// L = 76
