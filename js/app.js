9e515b2a997299d56fd70066d20431fe3dbf8744
Daniel-R-Mott <daniel.r.mott@gmail.com>
2024-03-12 19:42:34 -0700

    updated functions, css transitions

--- js/app.js
+++ js/app.js
@@ -1,36 +1,19 @@
-document.addEventListener("keydown", checkUpAndDown, false);
-document.addEventListener("keyup", checkUpAndDown, false);
-
-// Takes str from keydown and adds it to "#key", then toggles the classes for that key id
-function checkUpAndDown(e) {
-  if (e.repeat) {
-    return;
-  }
-  const selectorString = "#key" + String.fromCharCode(e.keyCode);
-  const pressedKeyElement = document.querySelector(selectorString);
-  pressedKeyElement.classList.toggle("key");
-  pressedKeyElement.classList.toggle("key-pressed");
-  console.log(e.keyCode);
-
-
-
-  if (pressedKeyElement.classList.contains("key-pressed")) {
-    playAudio(e.keyCode);
-  }
-};
-
-function playAudio(keyCode) {
-  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
+  key.classList.add('key-pressed');
+  console.log(key);
 };
 
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('key-pressed')
+};
 
-// S = 83
-// D = 68
-// F = 70
-// G = 71
-// H = 72
-// J = 74
-// K = 75
-// L = 76
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
