c8f4ddf4879ffe5ff546b67d4862765c72250ced
Daniel-R-Mott <daniel.r.mott@gmail.com>
2024-03-13 20:35:09 -0700

    added mobile styles and touch response

--- js/app.js
+++ js/app.js
@@ -1,20 +1,34 @@
 function playSound(e) {
-  if (e.repeat)
-  return;
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+ /* if (e.repeat)
+  return;*/
+  const audio = document.querySelector(`audio[data-key="${e}"]`);
+  const key = document.querySelector(`.key[data-key="${e}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add('key-pressed');
-};
+}
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
-  this.classList.remove('key-pressed')
-};
+  this.classList.remove('key-pressed');
+}
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => {
+  key.addEventListener('touchstart', playSound);
+  key.addEventListener('transitionend', removeTransition);
+});
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', getKeyCode);
+
+
+function getDataKey(d) {
+const dataKey = d.getAttribute("data-key");
+playSound(dataKey);
+}
+
+function getKeyCode(e) {
+  const keyCode = e.keyCode;
+  playSound(keyCode);
+}
